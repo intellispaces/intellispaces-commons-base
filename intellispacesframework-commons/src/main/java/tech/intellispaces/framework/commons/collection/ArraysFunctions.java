91f79874b9abb1a9f68f20aba3aafe7694644293
lavor <lavor.andrey@gmail.com>
2024-07-17 12:14:24 +0600

    New methods added to class ArraysFunctions

--- intellispacesframework-commons/src/main/java/tech/intellispaces/framework/commons/collection/ArraysFunctions.java
+++ intellispacesframework-commons/src/main/java/tech/intellispaces/framework/commons/collection/ArraysFunctions.java
@@ -1,5 +1,8 @@
 package tech.intellispaces.framework.commons.collection;
 
+import java.lang.reflect.Array;
+import java.util.function.Function;
+
 /**
  * Arrays processing functions.
  */
@@ -23,5 +26,40 @@ public interface ArraysFunctions {
       }
     }
     return false;
+  }
+
+  static <E, V> boolean contains(E[] array, Function<E, V> mapper, V value) {
+    if (array == null) {
+      return false;
+    }
+    if (value == null) {
+      for (E e : array) {
+        if (e == null) {
+          return true;
+        }
+      }
+    } else {
+      for (E e : array) {
+        if (mapper.apply(e).equals(value)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  @SuppressWarnings("unchecked")
+  static <S, T> T[] map(S[] array, Function<S, T> mapper, Class<T> targetClass) {
+    if (array == null) {
+      return null;
+    }
+    if (array.length == 0) {
+      return (T[]) Array.newInstance(targetClass, 0);
+    }
+    T[] result = (T[]) Array.newInstance(targetClass, array.length);
+    for (int i = 0; i < array.length; i++) {
+      result[i] = mapper.apply(array[i]);
+    }
+    return result;
   }
 }
--- intellispacesframework-commons/src/test/java/tech/intellispaces/framework/commons/collection/ArraysFunctionsTest.java
+++ intellispacesframework-commons/src/test/java/tech/intellispaces/framework/commons/collection/ArraysFunctionsTest.java
@@ -16,7 +16,22 @@ public class ArraysFunctionsTest {
 
     assertThat(ArraysFunctions.contains(new String[] {"a", "b", "c"}, null)).isFalse();
     assertThat(ArraysFunctions.contains(new String[] {"a", null, "c"}, null)).isTrue();
-
     assertThat(ArraysFunctions.contains(new String[] {"a", "b", "c"}, "c")).isTrue();
+  }
+
+  @Test
+  public void testContainsWithMapper() {
+    assertThat(ArraysFunctions.<String, Integer> contains(null, Integer::valueOf, 1)).isFalse();
+    assertThat(ArraysFunctions.contains(new String[0], Integer::valueOf, 1)).isFalse();
+
+    assertThat(ArraysFunctions.contains(new String[] {"0", "1", null}, Integer::valueOf, null)).isTrue();
+    assertThat(ArraysFunctions.contains(new String[] {"0", "1", "2"}, Integer::valueOf, 1)).isTrue();
+  }
+
+  @Test
+  public void testMap() {
+    assertThat(ArraysFunctions.<String, Integer> map(null, Integer::valueOf, Integer.class)).isNull();
+    assertThat(ArraysFunctions.map(new String[0], Integer::valueOf, Integer.class)).isEmpty();
+    assertThat(ArraysFunctions.map(new String[] {"1", "2", "3"}, Integer::valueOf, Integer.class)).contains(1, 2, 3);
   }
 }
