205b103d13446cdfb6cf29190675e4214e24d77c
lavor <lavor.andrey@gmail.com>
2024-10-11 11:32:11 +0600

    Conversion functions added to MathFunctions

--- intellispaces-base/src/main/java/intellispaces/common/base/math/MathFunctions.java
+++ intellispaces-base/src/main/java/intellispaces/common/base/math/MathFunctions.java
@@ -11,4 +11,20 @@ public interface MathFunctions {
     bb.putLong(uuid.getLeastSignificantBits());
     return bb.array();
   }
+
+  static int booleanToInt(boolean value) {
+    return value ? 1 : 0;
+  }
+
+  static long booleanToLong(boolean value) {
+    return value ? 1L : 0L;
+  }
+
+  static double booleanToDouble(boolean value) {
+    return value ? 1.0 : 0.0;
+  }
+
+  static boolean longToBoolean(long value) {
+    return value != 0L;
+  }
 }
--- intellispaces-base/src/test/java/intellispaces/common/base/math/MathFunctionsTest.java
+++ intellispaces-base/src/test/java/intellispaces/common/base/math/MathFunctionsTest.java
@@ -24,4 +24,31 @@ public class MathFunctionsTest {
         -84, -66, 19, -92, -123, 32, 68, 103, -88, 86, 46, -95, -119, 12, -104, -75
     });
   }
+
+  @Test
+  public void testBooleanToInt() {
+    assertThat(MathFunctions.booleanToInt(true)).isEqualTo(1);
+    assertThat(MathFunctions.booleanToInt(false)).isEqualTo(0);
+  }
+
+  @Test
+  public void testBooleanToLong() {
+    assertThat(MathFunctions.booleanToLong(true)).isEqualTo(1);
+    assertThat(MathFunctions.booleanToLong(false)).isEqualTo(0);
+  }
+
+  @Test
+  public void testBooleanToDouble() {
+    assertThat(MathFunctions.booleanToDouble(true)).isEqualTo(1.0);
+    assertThat(MathFunctions.booleanToDouble(false)).isEqualTo(0.0);
+  }
+
+  @Test
+  public void testLongToBoolean() {
+    assertThat(MathFunctions.longToBoolean(0L)).isFalse();
+
+    assertThat(MathFunctions.longToBoolean(1L)).isTrue();
+    assertThat(MathFunctions.longToBoolean(3L)).isTrue();
+    assertThat(MathFunctions.longToBoolean(-1L)).isTrue();
+  }
 }
